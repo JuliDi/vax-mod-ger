7f998f699190be3f9a5eca9375a77ec16f056839
JuliDi <20155974+JuliDi@users.noreply.github.com>
2021-04-17 17:42:11 +0200

    add more outputs, change from exp to x^3

--- extrapolate.jl
+++ extrapolate.jl
@@ -14,7 +14,9 @@ vaccinations = df[!,"personen_erst_kumulativ"]
 dates_future = [dates[1] + Day(i) for i in 0:200]
 dates_future_numeric = 0:200
 # Create exponential model function
-@. model(t, p) = p[1] * exp.(p[2] * t) .+ p[3]
+# @. model(t, p) = p[1] * exp.(p[2] * t) .+ p[3]
+# @. model(t, p) = p[1] * exp(p[2] * t) * t * t
+@. model(t, p) = p[1] * t^3 + p[2]
 
 # Think of some random start value, TODO: improve to use more realistic values
 p0 = [7.302e+05, 0.02773, 1000]
@@ -29,10 +31,17 @@ params_model = fit.param
 vax_extrapolated = model(dates_future_numeric, params_model)
 
 # find index where model predicts >80e6 vaccinations
-eightymio = findfirst(x -> x > 80e6, model(dates_future_numeric, params_model))
+eightymio = findfirst(x -> x > 73e6, model(dates_future_numeric, params_model))
 
 # Use plotly
 plotlyjs()
+
+# Make background transparent for export
+if isinteractive()
+    bg_col = :white
+else
+    bg_col = :transparent
+end
 
 # plot the curve!
 p = plot(dates, vaccinations,
@@ -43,7 +52,7 @@ p = plot(dates, vaccinations,
             ticks=:native,
             seriestype=:scatter,
             markersize=2,
-            background_color=:transparent,
+            background_color=bg_col,
             foreground_color=:black,
             linewidth=1,
             label="Actual Vaccinations",
@@ -56,17 +65,30 @@ plot!(
     dates_future,
     vax_extrapolated,
     linewidth=0.5,
-    label="Model",
+    label="Model (a * x³ + b)",
     ticks=:native)
 
 
 # Plot horizontal and vertical line for >80 Mio
 plot!(
     vcat(dates_future[1:eightymio], dates_future[eightymio]),
-    vcat(vax_extrapolated[eightymio] .* ones(eightymio), 0),
-    label=">80-Mio First-Dose",
+    vcat(vax_extrapolated[eightymio] * ones(eightymio), 0),
+    label=">73-Mio First-Dose",
     color=:green
     )
 
+if isinteractive()
+    display(p)
+end
+
+# Save the plot to a file
 savefig("Plot_Vax.html")
 
+# Create model params file
+res_file = open("results.html","w");
+println(res_file, "Model Parameters: <ul>")
+println(res_file, "<li>a = ", params_model[1], "</li>")
+println(res_file, "<li>b = ", params_model[2], "</li>")
+println(res_file, "</ul>")
+println(res_file, "<p>>73 Mio First-Dose Vaccinations on ", dates_future[eightymio], "</p>")
+close(res_file)
