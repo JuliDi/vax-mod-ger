9b15e31ca05d5de0f16627bc9308c36e5b0dbd7e
JuliDi <20155974+JuliDi@users.noreply.github.com>
2021-04-17 23:44:32 +0200

    switch to plotlyjs package

--- extrapolate.jl
+++ extrapolate.jl
@@ -1,4 +1,4 @@
-using Plots, LsqFit, CSV, DataFrames, Dates, HTTP
+using PlotlyJS, LsqFit, CSV, DataFrames, Dates, HTTP
 
 # Download CSV File
 println("Downloading new data...")
@@ -12,15 +12,19 @@ dates_numeric = Dates.value.(dates - dates[1])
 vaccinations = df[!,"personen_erst_kumulativ"]
 
 # Construct including future dates
-dates_future = [dates[1] + Day(i) for i in 0:200]
-dates_future_numeric = 0:200
+dates_future_numeric = 0:230
+dates_future = [dates[1] + Day(i) for i in dates_future_numeric]
+
 # Create exponential model function
 # @. model(t, p) = p[1] * exp.(p[2] * t) .+ p[3]
 # @. model(t, p) = p[1] * exp(p[2] * t) * t * t
-@. model(t, p) = p[1] * t^4 + p[2]
+# @. model(t, p) = p[1] * (t)^3 + p[3]
+function model(t, p)
+    p[1] .* (t .- p[2]).^3 .+ p[3]
+end
 
 # Think of some random start value, TODO: improve to use more realistic values
-p0 = [7.302e+05, 0.02773, 1000]
+p0 = [2.0, 2.0, 10000.0]
 
 # Fit the model
 fit = curve_fit(model, dates_numeric, vaccinations, p0)
@@ -32,10 +36,10 @@ params_model = fit.param
 vax_extrapolated = model(dates_future_numeric, params_model)
 
 # find index where model predicts >80e6 vaccinations
-eightymio = findfirst(x -> x > 73e6, model(dates_future_numeric, params_model))
+markline_idx = findfirst(x -> x > 73e6, vax_extrapolated)
 
 # Use plotly
-plotlyjs()
+# plotlyjs()
 
 # Make background transparent for export
 if isinteractive()
@@ -45,52 +49,52 @@ else
 end
 
 # plot the curve!
-p = plot(dates, vaccinations,
-            draw_arrow=true,
-            legend=:outertopright,
-            minorticks=true,
-            minorgrid=true,
-            ticks=:native,
-            seriestype=:scatter,
-            markersize=2,
-            background_color=bg_col,
-            foreground_color=:black,
-            linewidth=1,
-            label="Actual Vaccinations",
-            xlabel="Date (last update: " * string(Dates.format(now(), "Y-m-d, HH:MM") * ")"),
-            ylabel="Number of People with min. 1 Dose",
-            title="Vaccinations Model Germany",
-            options=Dict(:responsive => true)
-            # size=(1200,800)
+
+layout = Layout(
+    title="Vaccinations Model Germany<br>(last update: " * string(Dates.format(now(), "Y-m-d, HH:MM") * ")"),
+    xaxis=attr(
+        title=attr(text="Date"), showgrid=true,
+        zeroline=true),
+    yaxis=attr(title=attr(text="Number of People with min. 1 Dose", standoff=10), zeroline=false, automargin=true),
+);
+
+p_actual = scatter(
+    x=dates,
+    y=vaccinations,
+    mode="markers",
+    name="Actual Vaccinations",
+    marker=attr(symbol="circle-open")
 )
-plot!(
-    dates_future,
-    vax_extrapolated,
+
+p_model = scatter(
+    x=dates_future,
+    y=vax_extrapolated,
     linewidth=0.5,
-    label="Model (a * x³ + b)",
-    ticks=:native)
+    name="Model (a * x³ + b)",
+)
 
-
-# Plot horizontal and vertical line for >80 Mio
-plot!(
-    vcat(dates_future[1:eightymio], dates_future[eightymio]),
-    vcat(vax_extrapolated[eightymio] * ones(eightymio), 0),
-    label=">73-Mio First-Dose",
+# Plot horizontal and vertical line for >73 Mio
+p_lines = scatter(
+    x=vcat(dates_future[1:markline_idx], dates_future[markline_idx]),
+    y=vcat(vax_extrapolated[markline_idx] * ones(markline_idx), 0),
+    name=">73-Mio First-Dose",
     color=:green
-    )
+)
+
+p = plot([p_actual, p_model, p_lines], layout, options=Dict(:responsive => true, :editable => true))
 
 if isinteractive()
     display(p)
 end
 
 # Save the plot to a file
-savefig("Plot_Vax.html")
+savehtml(p, "Plot_Vax.html", :remote)
 
 # Create model params file
-res_file = open("results.html","w");
+res_file = open("results.html", "w");
 println(res_file, "Model Parameters: <ul>")
 println(res_file, "<li>a = ", params_model[1], "</li>")
 println(res_file, "<li>b = ", params_model[2], "</li>")
 println(res_file, "</ul>")
-println(res_file, "<p>>73 Mio First-Dose Vaccinations on ", dates_future[eightymio], "</p>")
+println(res_file, "<p>>73 Mio First-Dose Vaccinations on ", dates_future[markline_idx], "</p>")
 close(res_file)
